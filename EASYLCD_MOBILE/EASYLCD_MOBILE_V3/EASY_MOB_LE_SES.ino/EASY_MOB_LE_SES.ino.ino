d9e4f4012fe1ef2c77304f5c2ce56dab01c4153b
safaka123 <45589563+safaka123@users.noreply.github.com>
2022-07-29 16:33:07 +0300

    Add files via upload

---
+++ EASYLCD_MOBILE/EASYLCD_MOBILE_V3/EASY_MOB_LE_SES.ino/EASY_MOB_LE_SES.ino.ino
@@ -0,0 +1,75 @@
+// POT OKUMA İLe seri porttan gönder ve lcd r bekle
+// geldiğinde tekrar oku. Hızlı olmasın diye gecikme ver. 
+// 10 kez oku ortalama al ve 1 kez yaz. 
+// Arduino EASYMOBiL LCD VE BARKOD OKUYUCU. ARDUINO NANO,UNO KULLANILABİLİR
+//
+// DONANIMSAL SERİ PORT İLE BLUETOOTH BAĞLANACAK.
+ // #include <SoftwareSerial.h>
+  
+ // SoftwareSerial mySerial(10, 11); // RX, TX
+  int a = 0;   // değişken
+  String stringThree = "LCDWR 1,1-";
+  String YAZI ="";     
+  String readString="29.12.2021";    
+  float sensorValue;
+
+     
+  void setup() { //*****************************
+  //  mySerial.begin(9600);
+  //  delay(100);
+    
+    Serial.begin(9600);
+  //Serial.println("LCDDD");
+  stringThree="LCDDD";EkranYaz();
+   
+      delay(200);
+  }
+
+  void EkranYaz(){//*********************************************
+   tekrarla:
+   readString="";
+   int bul=-1;
+   int bull=-1;
+   int sergel=0;
+   Serial.print(stringThree);
+ bekle:
+   while (Serial.available()) {
+    delay(25);  
+    char c = Serial.read();  
+    readString += c;
+    sergel=1;
+  } 
+ 
+    bul=readString.indexOf("LCD R");
+    bull=readString.indexOf("LCD P");
+  if (bul>=0){
+  }else if (bull>=0){
+    delay(100);
+    goto tekrarla;
+  }else if (bul<0 && bull<0 && sergel==0){
+    delay(100);
+    goto bekle;
+  }else {
+    goto tekrarla; 
+  }
+    
+  delay(100);
+  
+    }
+ 
+ void ayikla(){//*********************************************
+
+   stringThree=("LCDWR 3,1-");stringThree=stringThree+sensorValue+" A";EkranYaz();
+   delay(20);
+ }
+  
+  void loop() {  //***************************************************  
+     
+      delay(100);
+      sensorValue = analogRead(A0);
+  sensorValue =sensorValue*(5.0/1023);
+   ayikla();    
+ delay(100);
+  }
+  
+
