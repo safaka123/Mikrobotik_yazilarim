6c5ca2a4b8ed64a655ae09e2145bfdbed42a5006
safaka123 <45589563+safaka123@users.noreply.github.com>
2023-02-27 16:53:21 +0300

    Delete ASISTANS/PHP dosyalar/api directory

--- ASISTANS/PHP dosyalar/api/firebase_MIT.php
+++
@@ -1,7 +0,0 @@
-<?php
-$homepage = file_get_contents('https://esp8266-4140a.firebaseio.com/bool_mit.json?auth=2NaRXXwRefCCxTx9KBarrDm957KKDJsN4nJj1Dqq');
-echo "bool_MIT=". $homepage;
-echo "<br>";
-
-
-?>
--- ASISTANS/PHP dosyalar/api/firebase_read1.php
+++
@@ -1,6 +0,0 @@
-<?php
-$homepage = file_get_contents('https://esp8266-4140a.firebaseio.com/bool.json?auth=2NaRXXwRefCCxTx9KBarrDm957KKDJsN4nJj1Dqq');
-echo "bool= $homepage";
-
-
-?>
